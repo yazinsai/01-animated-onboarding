bae251f8d8c961f7c012d243df1d7fd8036b48b8
Yazin <github-7itbcg5@yaz.in>
2020-09-15 15:53:27 +0300

    Refactor to files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:onboarding/widgets/animated_indicator.dart';
 
 const blue = Color(0xFF4781ff);
 
@@ -26,10 +25,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        brightness: Brightness.light,
-        title: Text("Aloha ☂"),
-      ),
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
             child: Column(
@@ -52,122 +48,47 @@ class ProgressButton extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
+  void onComplete() {
+    print('onComplete');
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 65,
       height: 65,
       child: Stack(children: [
-        AnimatedIndicator(duration: const Duration(seconds: 5), size: 65),
+        AnimatedIndicator(
+          duration: const Duration(seconds: 10),
+          size: 65,
+          callback: onComplete,
+        ),
         Center(
-          child: Container(
-            height: 50,
-            width: 50,
-            child: Center(
-              child: SvgPicture.asset(
-                "./assets/arrow.svg",
-                width: 10,
-              ),
-            ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(99), color: blue),
-          ),
+          child: NextButton(),
         )
       ]),
     );
   }
 }
 
-class AnimatedIndicator extends StatefulWidget {
-  final Duration duration;
-  final double size;
-  const AnimatedIndicator({Key key, this.duration, this.size})
-      : super(key: key);
-
-  @override
-  _AnimatedIndicatorState createState() => _AnimatedIndicatorState();
-}
-
-class _AnimatedIndicatorState extends State<AnimatedIndicator>
-    with TickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
-
-  @override
-  void initState() {
-    controller = AnimationController(duration: widget.duration, vsync: this);
-    animation = Tween(begin: 0.0, end: 100.0).animate(controller)
-      ..addListener(() {
-        setState(() {});
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reset();
-        }
-      });
-    controller.forward();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+class NextButton extends StatelessWidget {
+  const NextButton({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: animation,
-        builder: (context, child) {
-          return CustomPaint(
-              size: Size(widget.size, widget.size),
-              painter: ProgressPainter(animation.value));
-        });
+    return Container(
+      height: 50,
+      width: 50,
+      child: Center(
+        child: SvgPicture.asset(
+          "./assets/arrow.svg",
+          width: 10,
+        ),
+      ),
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(99), color: blue),
+    );
   }
-}
-
-class ProgressPainter extends CustomPainter {
-  final double progress;
-  ProgressPainter(this.progress);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    // setup
-    var linePaint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 3
-      ..color = blue;
-
-    var circlePaint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = blue;
-
-    final radians = (progress / 100) * 2 * pi;
-    _drawArc(canvas, linePaint, circlePaint, -pi / 2, radians, size);
-  }
-
-  void _drawArc(
-    Canvas canvas,
-    Paint linePaint,
-    Paint circlePaint,
-    double startRadian,
-    double sweepRadian,
-    Size size,
-  ) {
-    final centerX = size.width / 2, centerY = size.height / 2;
-    final centerOffset = Offset(centerX, centerY);
-    final double radius = min(size.width, size.height) / 2;
-
-    canvas.drawArc(Rect.fromCircle(center: centerOffset, radius: radius),
-        startRadian, sweepRadian, false, linePaint);
-
-    final x = radius * (1 + sin(sweepRadian)),
-        y = radius * (1 - cos(sweepRadian));
-    final circleOffset = Offset(x, y);
-    canvas.drawCircle(circleOffset, 5, circlePaint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter old) => true;
 }
---
+++ lib/widgets/animated_indicator.dart
@@ -0,0 +1,101 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+const blue = Color(0xFF4781ff);
+
+class AnimatedIndicator extends StatefulWidget {
+  final Duration duration;
+  final double size;
+  final VoidCallback callback;
+  const AnimatedIndicator({Key key, this.duration, this.size, this.callback})
+      : super(key: key);
+
+  @override
+  _AnimatedIndicatorState createState() => _AnimatedIndicatorState();
+}
+
+class _AnimatedIndicatorState extends State<AnimatedIndicator>
+    with TickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(duration: widget.duration, vsync: this);
+    animation = Tween(begin: 0.0, end: 100.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reset();
+          widget.callback();
+        }
+      });
+    controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return CustomPaint(
+              size: Size(widget.size, widget.size),
+              painter: ProgressPainter(animation.value));
+        });
+  }
+}
+
+class ProgressPainter extends CustomPainter {
+  final double progress;
+  ProgressPainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // setup
+    var linePaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3
+      ..color = blue;
+
+    var circlePaint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = blue;
+
+    final radians = (progress / 100) * 2 * pi;
+    _drawArc(canvas, linePaint, circlePaint, -pi / 2, radians, size);
+  }
+
+  void _drawArc(
+    Canvas canvas,
+    Paint linePaint,
+    Paint circlePaint,
+    double startRadian,
+    double sweepRadian,
+    Size size,
+  ) {
+    final centerX = size.width / 2, centerY = size.height / 2;
+    final centerOffset = Offset(centerX, centerY);
+    final double radius = min(size.width, size.height) / 2;
+
+    canvas.drawArc(Rect.fromCircle(center: centerOffset, radius: radius),
+        startRadian, sweepRadian, false, linePaint);
+
+    final x = radius * (1 + sin(sweepRadian)),
+        y = radius * (1 - cos(sweepRadian));
+    final circleOffset = Offset(x, y);
+    canvas.drawCircle(circleOffset, 5, circlePaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter old) => true;
+}
