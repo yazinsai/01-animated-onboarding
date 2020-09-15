237a1b6170d3c1fb74eaae06a444b237aacd34e8
Yazin <github-7itbcg5@yaz.in>
2020-09-15 14:42:13 +0300

    Static progress indicator

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# onboarding
+# Animated Onboarding
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Reference video for understanding `sin`, `cos` and how they relate to the horizontal and vertical positions of a point on a circle: https://www.youtube.com/watch?v=a_zReGTxdlQ
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+const blue = Color(0xFF4781ff);
 
 void main() {
   runApp(MyApp());
@@ -10,108 +15,110 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MainPage extends StatelessWidget {
+  const MainPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        brightness: Brightness.light,
+        title: Text("Aloha ☂"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      body: SafeArea(
+        child: Container(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset("./assets/hero-1.png"),
+            Text("Boost your traffic"),
+            Text("Outreach to many social networks to improve your statistics"),
+            ProgressButton(),
+            Text("Skip")
           ],
-        ),
+        )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class ProgressButton extends StatelessWidget {
+  const ProgressButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 65,
+      height: 65,
+      child: Stack(children: [
+        CustomPaint(size: Size(65, 65), painter: ProgressPainter(20.0)),
+        Center(
+          child: Container(
+            height: 50,
+            width: 50,
+            child: Center(
+              child: SvgPicture.asset(
+                "./assets/arrow.svg",
+                width: 10,
+              ),
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(99), color: blue),
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class ProgressPainter extends CustomPainter {
+  final double progress;
+  ProgressPainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // setup
+    var linePaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3
+      ..color = blue;
+
+    var circlePaint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = blue;
+
+    final radians = (progress / 100) * 2 * pi;
+    _drawArc(canvas, linePaint, circlePaint, -pi / 2, radians, size);
+  }
+
+  void _drawArc(
+    Canvas canvas,
+    Paint linePaint,
+    Paint circlePaint,
+    double startRadian,
+    double sweepRadian,
+    Size size,
+  ) {
+    final centerX = size.width / 2, centerY = size.height / 2;
+    final centerOffset = Offset(centerX, centerY);
+    final double radius = min(size.width, size.height) / 2;
+
+    canvas.drawArc(Rect.fromCircle(center: centerOffset, radius: radius),
+        startRadian, sweepRadian, false, linePaint);
+
+    final x = radius * (1 + sin(sweepRadian)),
+        y = radius * (1 - cos(sweepRadian));
+    final circleOffset = Offset(x, y);
+    canvas.drawCircle(circleOffset, 5, circlePaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter old) => false;
 }
